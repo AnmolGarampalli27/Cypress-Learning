bdde91de3dc8ad49f37af2d968e7c8c508564756
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-11-03 15:04:24 +0530

    Assertions

--- cypress/e2e/AssertionsTitle.cy.js
+++ cypress/e2e/AssertionsTitle.cy.js
@@ -9,8 +9,24 @@ describe( "Assertions Tutorial", ()=>{
         .and('eq','OrangeHRM')
         .and ('not.contain','orangee') //Negative scenario
 
-    // checking the presence of an image using should keyword   
+    // checking the presence of an image using should & be visisble keyword   
         cy.get('.orangehrm-login-branding > img').should('be.visible')
-        
+
+    // checking the presence of an image using should & exist keyword   
+        cy.get('.orangehrm-login-branding > img').should('exist')
+
+        // The above code can be further optimized
+        cy.get('.orangehrm-login-branding > img').should('be.visible')
+        .and('exist')
+
+        //provide a value in the input box
+        cy.get("input[placeholder='Username']").type("Admin") 
+
+        //Check if the value is filled in the correct field
+        cy.get("input[placeholder='Username']").should('have.value','Admin')
+
+        //Optimizing the above code
+        .should('have.value','Admin')
+
     })
 })
