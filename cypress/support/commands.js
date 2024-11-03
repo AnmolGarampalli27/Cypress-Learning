abcc5c4561075a02e026cc2f6a917c14bc899ea5
AnmolGarampalli27 <anmolgarampalli@gmail.com>
2024-11-03 17:52:41 +0530

    Added comments to the commands.js file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,5 +26,5 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-/// <reference types="cypress"/>
-/// <reference types="cypress-xpath"/>
+/// <reference types="cypress"/> //for cypress
+/// <reference types="cypress-xpath"/> //for xpath
